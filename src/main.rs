af30fea49b7ab0456fe33bdf24f6eca79e815143
Bozhan Liang <lbz@rivensbane.com>
2026-03-08 16:57:33 +1300

    Fix progress bar display issue

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-use std::path::Path;
+use std::{path::Path, sync::Mutex};
 
-use futures_util::{stream, stream::StreamExt as _};
+use futures_util::stream::{self, StreamExt as _};
 use indicatif::{MultiProgress, ProgressBar};
 use reqwest::{Body, Version};
-use tokio::{fs::File, io::BufReader};
+use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 
 mod configs;
 use configs::{API_URL, ARGS, CLIENT, PROGRESS_STYLE};
 
+// set_move_cursor plays badly with println
+// https://github.com/console-rs/indicatif/issues/759
+static PRINT_LOCK: Mutex<()> = Mutex::new(());
+
 #[tokio::main]
 async fn main() {
     let bars = MultiProgress::new();
-    bars.set_move_cursor(true);
 
+    // https://github.com/console-rs/indicatif/issues/447
+    let dummy_bar = bars.add(ProgressBar::no_length());
     bars.println(format!(
         "Uploading {} files with {} workers",
         ARGS.files.len(),
@@ -31,11 +36,28 @@ async fn main() {
     ))
     .unwrap();
 
+    dummy_bar.finish();
+    bars.remove(&dummy_bar);
+    drop(dummy_bar);
+
     stream::iter(ARGS.files.iter().map(Path::new))
         .for_each_concurrent(ARGS.workers.get(), |file| {
             let bars = bars.clone();
-            let bar = bars.add(ProgressBar::no_length());
-            bar.set_style(PROGRESS_STYLE.clone());
+
+            let bar = {
+                let len = file.metadata().unwrap().len();
+
+                let _lock = PRINT_LOCK.lock().unwrap();
+                bars.set_move_cursor(false);
+
+                let bar = bars.add(ProgressBar::new(len));
+                bar.set_style(PROGRESS_STYLE.clone());
+
+                bars.set_move_cursor(true);
+                drop(_lock);
+
+                bar
+            };
 
             let file_name = file.file_name().unwrap();
             let file_name = file_name.to_str().unwrap();
@@ -52,10 +74,7 @@ async fn upload_one(file: &Path, bar: ProgressBar, bars: MultiProgress) {
     let url = API_URL.join(file_name).unwrap();
 
     let file = File::open(&file).await.unwrap();
-    let file_size = file.metadata().await.unwrap().len();
-    bar.set_length(file_size);
-    let reader_stream =
-        ReaderStream::with_capacity(bar.wrap_async_read(BufReader::new(file)), 65536);
+    let reader_stream = ReaderStream::with_capacity(bar.wrap_async_read(file), 64 * 1024);
 
     let resp = CLIENT
         .put(url)
@@ -67,16 +86,22 @@ async fn upload_one(file: &Path, bar: ProgressBar, bars: MultiProgress) {
         .unwrap();
 
     if !resp.status().is_success() {
-        bars.println(format!(
-            "Failed to upload {}: {}",
-            file_name,
-            resp.text().await.unwrap()
-        ))
-        .unwrap();
-        panic!("Failed to upload {}", file_name);
+        let text = resp.text().await.unwrap();
+        bars.suspend(|| {
+            eprintln!("Failed to upload {}: {}", file_name, text);
+            panic!("Failed to upload {}", file_name);
+        });
     }
 
-    bar.finish_and_clear();
-    bars.remove(&bar);
-    bars.println(format!("Uploaded {}", file_name)).unwrap();
+    {
+        let _lock = PRINT_LOCK.lock().unwrap();
+        bars.set_move_cursor(false);
+
+        bar.finish();
+        bars.remove(&bar);
+        bars.println(format!("Uploaded {}", file_name)).unwrap();
+
+        bars.set_move_cursor(true);
+        drop(_lock);
+    };
 }
